dc025aab4a333f14749d6000c6e1daaa09a85417
Anee <mananee8@gmail.com>
2015-10-10 16:08:44 +0530

    MTI emhancment fixes

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,4 @@
 class Business < ActiveRecord::Base
   acts_as_profile
-
-  self.per_page = 20
   accepts_nested_attributes_for :user
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,5 @@
 class Customer < ActiveRecord::Base
   acts_as_profile
-
-  self.per_page = 20
   accepts_nested_attributes_for :user
 
   validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
